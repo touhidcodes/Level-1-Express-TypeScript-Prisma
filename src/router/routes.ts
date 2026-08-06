70f2fa5183d5fc52d2d7f08fb9abc69159721b2d
Touhidur Zaman <touhidcodes@gmail.com>
2026-08-06 20:00:10 +0600

    mod: package.json

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,14 +1,14 @@
 import { Router } from "express";
 
-import userRoutes from "./users";
-import productRoutes from "./products";
+// import userRoutes from "./users";
+// import productRoutes from "./products";
 
 const router = Router();
 
 // User Routes
-router.use("/users", userRoutes);
+// router.use("/users", userRoutes);
 
 // Product Routes
-router.use("/products", productRoutes);
+// router.use("/products", productRoutes);
 
 export default router;
