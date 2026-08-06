33c4d3a39a27dc48c8eecafe9024749ffbf179bc
Touhidur Zaman <touhidcodes@gmail.com>
2026-08-06 19:43:54 +0600

    add: routes file

---
+++ src/router/routes.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+
+import userRoutes from "./users";
+import productRoutes from "./products";
+
+const router = Router();
+
+// User Routes
+router.use("/users", userRoutes);
+
+// Product Routes
+router.use("/products", productRoutes);
+
+export default router;
