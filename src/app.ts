9a48a669b29ed0505f1afc6ded62afecc78f67cd
Touhidur Zaman <touhidcodes@gmail.com>
2026-08-06 19:42:24 +0600

    add: app file

---
+++ src/app.ts
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+
+import routes from "./routes";
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Home Route
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Welcome to Prisma Express Starter API 🚀",
+  });
+});
+
+// API Routes
+app.use("/api/v1", routes);
+
+// 404 Route
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Route Not Found",
+  });
+});
+
+export default app;
