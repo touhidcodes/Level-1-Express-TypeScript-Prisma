d01eef08575438798e7860e7e737355f2188e5d5
Touhidur Zaman <touhidcodes@gmail.com>
2026-08-06 19:48:43 +0600

    add: server file

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+import app from "./app";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+const startServer = async () => {
+  try {
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
